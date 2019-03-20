8a7ab268d03a099e10971999e8740dcce35659b9
Shizukanawa <deviousfinest@gmail.com>
2019-03-20 15:25:33 +0100

    Fixed settings and made connection from C# to SUMO

--- ProbabilistiskModellering/Program.cs
+++ ProbabilistiskModellering/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CodingConnected.TraCI.NET;
 
 namespace ProbabilistiskModellering
 {
@@ -10,10 +11,24 @@ namespace ProbabilistiskModellering
     {
         static void Main(string[] args)
         {
-            void Test(string test)
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        public static async Task MainAsync(string[] args)
+        {
+            await Task.Run(async () =>
             {
-                return test;
-            }
+                var client = new TraCIClient();
+                Console.Write("Insert Port Number: ");
+                int port = int.Parse(Console.ReadLine());
+                await client.ConnectAsync("127.0.0.1", port);
+                while (true)
+                {
+                    Console.WriteLine("Taking a step");
+                    client.Control.SimStep();
+                    await Task.Delay(100);
+                }
+            });
         }
     }
 }
