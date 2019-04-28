bdcc640a1eac0c8935cab8e6f2ba7aa965cb5e16
rherum18 <44167651+rherum18@users.noreply.github.com>
2019-04-28 17:14:31 +0200

    fixed unittests

--- UnitTests/DNATest.cs
+++ UnitTests/DNATest.cs
@@ -1,10 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProbabilistiskModellering;
 
 namespace UnitTests
 {
+    [TestClass]
     class DNATest
     {
+        [TestMethod]
+        public void GetSpecificXMLAttributeFromFile_ExpectedFirstArrayElement_ReturnsArray()
+        {
+            // Arrange
+            DNA<string> test = new DNA<string>(int size, Func<T>);
+            string filePath = "./TestFiles/XMLTest.xml";
+            string[] result = new string[1];
+
+            // Act
+            result = test.GetSpecificXMLAttributeFromFile("tripinfo", "timeLoss", filePath);
+
+            // Assert
+            Assert.AreEqual(result[0], "6.78");
+
+        }
+
+        [TestMethod]
+        public void CalculateFitnessFunction_DoesCorrectCalculation_ReturnsDouble()
+        {
+            // Arrange
+            DNA<string> test = new DNA<string>();
+            string filePath = null;
+            string attribute = null;
+            string element = null;
+            string[] timeLossArray = test.GetSpecificXMLAttributeFromFile(element, attribute, filePath);
+            int cars = timeLossArray.Count();
+            double timeLossSum = 0.0;
+            for (int i = 0; i < cars; i++)
+            {
+                timeLossSum += double.Parse(timeLossArray[i], CultureInfo.InvariantCulture);
+            }
+            double result = new double();
+
+            // Act
+            result = test.CalculateFitnessFunction(test, element, attribute, filePath);
+
+            // Assert
+            Assert.
+
+        }
     }
 }
